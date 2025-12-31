ac9c54ae63ba1ffae5c1c68e6fa5f3f03bf18b3c
unknown <zunairawan4343@gmail.com>
2025-12-31 16:08:59 +0500

    modify API

--- apps.js
+++ apps.js
@@ -2,6 +2,8 @@ const API_URL = "https://latest.currency-api.pages.dev/v1/currencies";
 const fromSelect = document.getElementById("from");
 const toSelect = document.getElementById("to");
 const output = document.getElementById("output");
+const convertBtn = document.getElementById("convertBtn");
+
 
 async function loadCurrencies() {
     try {
@@ -20,26 +22,38 @@ async function loadCurrencies() {
         toSelect.value = "pkr";
     } catch (error) {
         console.error("Error loading currencies", error);
+        output.innerText = "Failed to load currencies!";
     }
 }
-loadCurrencies();
+
 
 async function convertCurrency() {
-    const amount = document.getElementById("amount").value;
+    const amount = document.getElementById("amount").value.trim();
     const from = fromSelect.value;
     const to = toSelect.value;
-    if (amount === "") {
-        output.innerText = "Please enter amount";
+
+    if (!amount || isNaN(amount) || Number(amount) <= 0) {
+        output.innerText = "Please enter a valid amount";
         return;
     }
+
     try {
         const response = await fetch(`${API_URL}/${from}.json`);
         const data = await response.json();
         const rate = data[from][to];
+        if (!rate) {
+            output.innerText = `Conversion rate not available for ${from} to ${to}`;
+            return;
+        }
         const convertedAmount = (amount * rate).toFixed(2);
         output.innerText = `${amount} ${from.toUpperCase()} = ${convertedAmount} ${to.toUpperCase()}`;
     } catch (error) {
-        output.innerText = "Error fetching data!";
+        output.innerText = "Error fetching conversion!";
         console.error(error);
     }
 }
+
+
+loadCurrencies();
+
+convertBtn.addEventListener("click", convertCurrency);
